e11a5cee3d6d2adf4ee52c01eeb240533e4befd3
Matt Harrison <matt.harrison@sap.com>
2014-12-09 10:57:17 +0000

    Added comments

--- fileutils.go
+++ fileutils.go
@@ -4,6 +4,7 @@ import (
 	"os"
 )
 
+// A simple wrapper that checks if a file exists and returns a boolean
 func FileExists(filename string) bool {
 	if _, err := os.Stat(filename); err == nil {
 		return true
