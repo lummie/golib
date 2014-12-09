317f681cddb05147da0bfe1f4a3a50c5d187a549
Matt Harrison <matt.harrison@sap.com>
2014-12-09 11:30:49 +0000

    Added unit test for fileutils

---
+++ fileutils_test.go
@@ -0,0 +1,37 @@
+package goUtils
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestFileThatDoesNotExist(t *testing.T) {
+	filename := "/tmp/FileThatShouldNotExist"
+	exists := FileExists(filename)
+	if exists {
+		t.Error("Filename should not exist", filename)
+	}
+}
+
+func TestFileThatDoesExist(t *testing.T) {
+	filename := "/tmp/FileThatShouldNotExist"
+
+	// create the file
+	err := ioutil.WriteFile(filename, []byte("hello"), 0644)
+	if err != nil {
+		t.Error("Failed to create file", filename)
+	}
+
+	// check it exists
+	exists := FileExists(filename)
+	if !exists {
+		t.Error("Filename should exist", filename)
+	}
+
+	// clean up file after test
+	err = os.Remove(filename)
+	if err != nil {
+		t.Error("Failed to remove the file", filename)
+	}
+}
